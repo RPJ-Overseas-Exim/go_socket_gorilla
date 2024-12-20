30c51f1545e025baca6e145349bf97b42f86b185
Vikrant <tiwarivikrant9764@gmail.com>
2024-12-20 11:06:22 +0530

    implemented the chat message sending eventlistener and message appending in the messages

--- static/js/chat.mjs
+++ static/js/chat.mjs
@@ -17,11 +17,56 @@ function connectToSocket() {
     conn.onclose = function(){
         console.log("socket disconnected")
     }
+
+    return conn
+}
+
+function sendMessage(conn){
+    const messageForm = document.querySelector(".message-form")
+    const messageInput = document.querySelector(".message-input")
+
+    if(messageForm){
+        messageForm.addEventListener("submit", async (e)=>{
+            e.preventDefault()
+            const data = new FormData(e.target)
+            const dataObject = Object.fromEntries(data.entries())
+            
+            if(dataObject?.message.length > 0){
+                conn.send(dataObject.message)
+                appendMessage(dataObject.message)
+            }
+
+            messageInput.value = ""
+        })
+    }
+}
+
+function appendMessage(msg){
+
+    const messageBox = document.createElement("div")
+    messageBox.className = "pb-4 px-4 flex justify-end"
+    const message = document.createElement("div")
+    message.className = "border border-border px-4 py-1 rounded-lg rounded-br-[0px]"
+    message.innerText = msg
+    messageBox.appendChild(message)
+
+    const messages = document.querySelector("#messages")
+    if(messages){
+        messages.appendChild(messageBox)
+    }
+
 }
 
 if( location.pathname === "/admin"){
-    connectToSocket()
+    const conn = connectToSocket()
+
+    document.addEventListener("htmx:afterRequest", function (e){
+        if(e.detail.target.id == "chat-messages"){
+            sendMessage(conn)
+        }
+    })
 }
+
 
 //const chatBtns = document.querySelectorAll(".chat-button")
 //
